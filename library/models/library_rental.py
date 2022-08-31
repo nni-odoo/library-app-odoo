7d3a98e3b0ff672b0fbcdeba5d7fc56de4069526
nni-odoo <nni@odoo.com>
2022-08-31 15:08:20 +0800

    ORM Methods

--- library/models/library_rental.py
+++ library/models/library_rental.py
@@ -1,4 +1,7 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+from dateutil.relativedelta import relativedelta
+import datetime
 
 class LibraryRental(models.Model):
     _name = "library.rental"
@@ -8,3 +11,27 @@ class LibraryRental(models.Model):
     book = fields.Many2one("library.book")
     date_rent = fields.Date("Rental Date")
     date_end = fields.Date("Max Return Date")
+
+    overdue_fee = fields.Float("Overdue Fee", compute="_compute_overdue_fee")
+
+    def _compute_overdue_fee(self):
+        today = fields.Date.today()
+        for rec in self:
+            rec['overdue_fee'] = 0
+            if rec.date_end and today > rec.date_end:
+                days_overdue = (today - rec.date_end).days
+                rec['overdue_fee'] = days_overdue * 2
+
+    @api.constrains('date_rent', 'date_end')
+    def _constraint_date(self):
+        for rec in self:
+            if rec.date_rent and rec.date_end and rec.date_end < rec.date_rent:
+                raise ValidationError("End date should be later than the start date")
+
+    @api.model
+    def create(self, vals):
+        today = fields.Date.today()
+        end_day = today + relativedelta(months=1)
+        vals['date_rent'] = today
+        vals['date_end'] = end_day
+        return super().create(vals)
